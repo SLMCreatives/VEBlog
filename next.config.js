b45eee910d4c8544b22294fb81bd85174baaffe6
SLMCreatives <sulaiman@slmcreatives.com>
2024-04-14 18:53:18 +0800

    config2

--- next.config.js
+++ next.config.js
@@ -7,11 +7,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app www.googletagmanager.com 'strict-dynamic' https: http:;
-  base-uri 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app www.googletagmanager.com;
   style-src 'self' 'unsafe-inline';
   img-src * www.googletagmanager.com blob: data:;
-  media-src * .s3.amazonaws.com ;
+  media-src * .s3.amazonaws.com;
   connect-src *;
   font-src 'self';
   frame-src giscus.app
